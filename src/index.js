7d511b0ddec0eb683047ffc5882b187a6c7b9bfc
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-27 16:45:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -27,5 +27,8 @@ Order.propTypes = {
   cone: PropTypes.bool,
   size: PropTypes.string,
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
-  orderInfo: PropTypes.object.isRequired
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
 };
